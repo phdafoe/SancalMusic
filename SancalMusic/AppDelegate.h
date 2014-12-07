387bebd81a8ae95587b1036430429e44470ba832
User <David@MacBook-Pro.local>
2014-12-07 12:24:11 +0100

    Initial Commit

---
+++ SancalMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SancalMusic
+//
+//  Created by User on 7/12/14.
+//  Copyright (c) 2014 iCologic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
